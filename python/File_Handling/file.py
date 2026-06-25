85cc2625458442e188fb38c8d30f173378447e9c
Bhuvaneshwaran <bhuvaneshwaran7867@gmail.com>
2026-06-25 10:49:54 +0530

    practicing regex

---
+++ python/File_Handling/file.py
@@ -0,0 +1,7 @@
+#with open(r"C:\Users\user\Pictures\Desktop\Data_Analytics_Portfolio\Python1\File_Handling\Text_file") as file:
+    #data= file.read()
+   # print(data)
+
+with open(r"C:\Users\user\Pictures\Desktop\Data_Analytics_Portfolio\Python1\File_Handling\Text_file",'w') as file:
+        file.write("Bhuvi")
+        
